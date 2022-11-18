f42761b171254875e5c4370d81b8fdb6c888a0ed
SolcAndMe <97065474+SolcAndMe@users.noreply.github.com>
2022-11-18 03:40:23 +0000

    Update content of Resource Modal

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,31 +1,36 @@
 import { Link, Modal, Text } from '@nextui-org/react';
 import React from 'react';
 
-const links = [{
-  text: 'FUD Balancer pool',
-  url: 'https://app.balancer.fi/#/ethereum/pool/0x178e029173417b1f9c8bc16dcec6f697bc32374600000000000000000000025d',
-}, {
-  text: 'FIAT (etherscan)',
-  url: 'https://etherscan.io/address/0x586Aa273F262909EEF8fA02d90Ab65F5015e0516',
-}, {
-  text: 'Discord',
-  url: 'https://discord.gg/fiatdao',
-}, {
-  text: 'Fiat Dao',
-  url: 'https://fiatdao.com/',
-}, {
-  text: 'Fiat Docs',
-  url: 'https://docs.fiatdao.com',
-}, {
-  text: 'Fiat App',
-  url: 'https://app.fiatdao.com',
-}, {
-  text: 'Fiat Gov',
-  url: 'https://gov.fiatdao.com',
-}, {
-  text: '@fiatDao Github',
-  url: 'https://github.com/fiatdao',
-}]
+const links = [
+  {
+    text: 'Old App',
+    url: 'https://app.fiatdao.com',
+  },
+  {
+    text: 'FIAT-USDC-DAI Balancer Pool',
+    url: 'https://app.balancer.fi/#/ethereum/pool/0x178e029173417b1f9c8bc16dcec6f697bc32374600000000000000000000025d',
+  },
+  {
+    text: 'Website',
+    url: 'https://fiatdao.com/',
+  },
+  {
+    text: 'Discord',
+    url: 'https://discord.gg/fiatdao',
+  },
+  {
+    text: 'Governance',
+    url: 'https://gov.fiatdao.com',
+  },
+  {
+    text: 'Documentation',
+    url: 'https://docs.fiatdao.com',
+  },
+  {
+    text: 'Github',
+    url: 'https://github.com/fiatdao',
+  }
+];
 
 const linkCss = {
   fontFamily: 'var(--rk-fonts-body)',
@@ -50,7 +55,7 @@ export const InfoModal = (props: any) => {
     >
       <Modal.Header css={{justifyContent: 'space-between'}}>
         <Text id="modal-title" size={18} css={headerCss}>
-          Fiat Info
+          Resources
         </Text>
       </Modal.Header>
       <Modal.Body css={{paddingTop: '0px'}}>
