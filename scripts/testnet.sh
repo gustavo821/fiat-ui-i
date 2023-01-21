229fa13bee4e1dc13ca611899e2f7ae6042e817a
jFarms <jfarmscrypto@gmail.com>
2023-01-21 10:36:53 -0500

    Fix weird indentation

--- scripts/testnet.sh
+++ scripts/testnet.sh
@@ -16,9 +16,9 @@ fi
 
 # hardcoded unlocked accounts hold USDC, DAI and ETH necessary for testing on mainnet
 cmd="npx ganache \
-	--fork.url=https://eth-mainnet.alchemyapi.io/v2/$NEXT_PUBLIC_ALCHEMY_API_KEY \
-	--miner.defaultGasPrice 30000000000 \
-	--chain.vmErrorsOnRPCResponse=true \
+  --fork.url=https://eth-mainnet.alchemyapi.io/v2/$NEXT_PUBLIC_ALCHEMY_API_KEY \
+  --miner.defaultGasPrice 30000000000 \
+  --chain.vmErrorsOnRPCResponse=true \
   --wallet.unlockedAccounts=0xCFFAd3200574698b78f32232aa9D63eABD290703 \
   --wallet.unlockedAccounts=0x16b34ce9a6a6f7fc2dd25ba59bf7308e7b38e186 \
   --wallet.unlockedAccounts=0xF1A7dA08F6cb83069817d2D8F6e55E4F2D6C0834 "
